4d1078705ba7e4a72d1957db100d8b47e98f65f5
Jasmeet (Jazz) Bhatia <jasmeet.bhatia.us@gmail.com>
2026-04-03 20:47:54 -0700

    boilerplate for #35

---
+++ 35. Search Insert Position/src/main.rs
@@ -0,0 +1,10 @@
+pub fn search_insert(nums: Vec<i32>, target: i32) -> i32 {
+    return 0;
+}
+
+fn main() {
+    let nums = vec![1,3,5,6];
+    let target = 5;
+    let x = search_insert(nums, target);
+    println!("Answer: {}",x);
+}
