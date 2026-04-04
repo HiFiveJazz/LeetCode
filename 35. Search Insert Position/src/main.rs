b07f7805fd419d5d383184bf2667fa8ab11e3ab1
Jasmeet (Jazz) Bhatia <jasmeet.bhatia.us@gmail.com>
2026-04-03 21:36:57 -0700

    working #35 with binary search optimization, and pointer exmaple

--- 35. Search Insert Position/src/main.rs
+++ 35. Search Insert Position/src/main.rs
@@ -1,16 +1,37 @@
 pub fn search_insert(nums: Vec<i32>, target: i32) -> i32 {
-    // let larger_flag= false;
-    for i in 0..nums.len() {
-       if target <= nums[i] {
-            return i as i32;
-       }
+    let mut left = 0;
+    let mut right = nums.len();
+    while left < right {
+        let mid = (left + right) / 2;
+        if nums[mid] < target {
+            left = mid + 1;
+        } else {
+            right = mid;
+        }
     }
-    nums.len() as i32
+    left as i32
 }
+
+// NOTE: Alternative using pointers, but Rust no like
+// pub fn search_insert(nums: Vec<i32>, target: i32) -> i32 {
+//     let mut index = 0;
+//     let mut ptr = nums.as_ptr();
+//     let end = unsafe {ptr.add(nums.len())};
+//     while ptr < end {
+//         unsafe {
+//             if target <= *ptr {
+//                  return index;
+//             }
+//         }
+//         ptr = unsafe {ptr.add(1)};
+//         index += 1;
+//     }
+//     nums.len() as i32
+// }
 
 fn main() {
     let nums = vec![1,3,5,6];
-    let target = 1;
+    let target = 4;
     let x = search_insert(nums, target);
     println!("Answer: {}",x);
 }
