fd836bc542dc14f4749236e3b23fe2011989a9e4
Jasmeet (Jazz) Bhatia <jasmeet.bhatia.us@gmail.com>
2026-04-03 22:22:45 -0700

    add bitwise optimization (compiler might already optimize for this)

--- 35. Search Insert Position/src/main.rs
+++ 35. Search Insert Position/src/main.rs
@@ -2,7 +2,7 @@ pub fn search_insert(nums: Vec<i32>, target: i32) -> i32 {
     let mut left = 0;
     let mut right = nums.len();
     while left < right {
-        let mid = (left + right) / 2;
+        let mid = (left + right) >> 1;
         if nums[mid] < target {
             left = mid + 1;
         } else {
