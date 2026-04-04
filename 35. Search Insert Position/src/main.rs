47cb47331a6e5a8d65d13aa8a885638decf3652d
Jasmeet (Jazz) Bhatia <jasmeet.bhatia.us@gmail.com>
2026-04-03 20:59:25 -0700

    working, unoptimized 35

--- 35. Search Insert Position/src/main.rs
+++ 35. Search Insert Position/src/main.rs
@@ -1,10 +1,16 @@
 pub fn search_insert(nums: Vec<i32>, target: i32) -> i32 {
-    return 0;
+    // let larger_flag= false;
+    for i in 0..nums.len() {
+       if target <= nums[i] {
+            return i as i32;
+       }
+    }
+    nums.len() as i32
 }
 
 fn main() {
     let nums = vec![1,3,5,6];
-    let target = 5;
+    let target = 1;
     let x = search_insert(nums, target);
     println!("Answer: {}",x);
 }
