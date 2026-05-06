0701b137f2efd5c5cef371c2d0e4bb3fbc118bc8
Jasmeet (Jazz) Bhatia <jasmeet.bhatia.us@gmail.com>
2026-05-06 01:13:25 -0700

    added in some explanation and print statements for ease of understanding

--- 69. Sqrt(x)/src/main.rs
+++ 69. Sqrt(x)/src/main.rs
@@ -14,20 +14,33 @@ impl Solution {
         let mut answer = 1;
         while left <= right {
             let mid = left + (right - left) / 2; 
-            if mid <= x / mid {
+            println!("left:  {}",left);
+            println!("mid:   {}",mid);
+            println!("x/mid: {}",x/mid);
+            println!("right: {}",right);
+            // below we check if mid is a possible value for 
+            // the sqrt, if it is, we increment mid and check again
+            // if it is the case, we bump up our left to decrease 
+            // our search window
+            if mid <= x / mid {  
                 answer = mid;
                 left = mid + 1;
+                println!("mid less than x/mid, increase left");
+            // if it doesn't match, then our value is too big, so we 
+            // decrease our search window by 1 on the right
             } else {
                 right = mid - 1;
+                println!("mid greater than x/mid, decrease right");
             }
-
+            // Once left is bigger than right, we exit the loop.
+            println!("---------");
         }
         answer as i32
     }
 }
 
 fn main() {
-    let answer = Solution::my_sqrt(4);
-    println!("Answer: {}", answer);
+    let answer = Solution::my_sqrt(26);
+    println!("Answer:{}", answer);
 }
 
