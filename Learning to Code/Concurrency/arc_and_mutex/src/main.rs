3a8842f4345383d9ddd77435aef7efa4ab4b4d19
Jasmeet (Jazz) Bhatia <jasmeet.bhatia.us@gmail.com>
2026-05-15 01:11:04 -0700

    testing out concurrency stuff

--- Learning to Code/Concurrency/arc_and_mutex/src/main.rs
+++ Learning to Code/Concurrency/arc_and_mutex/src/main.rs
@@ -1,17 +1,50 @@
-use std::sync::{Arc, Mutex};
+// use std::sync::{Arc, Mutex};
+// use std::thread;
+//
+// fn main() {
+//     let counter = Arc::new(Mutex::new(0));
+//
+//     let mut handles  = vec![];
+//
+//     for i in 1..=2 {
+//         let counter_clone = Arc::clone(&counter);
+//
+//         let handle = thread::spawn(move || {
+//             loop {
+//                 let mut num = counter_clone.lock().unwrap();
+//                 if *num >= 100 {
+//                     break;
+//                 }
+//                 *num += 1;
+//                 println!("Thread {} increased counter to: {}", i, *num);
+//             }
+//         });
+//         handles.push(handle);
+//     }
+//
+//     for handle in handles {
+//         handle.join().unwrap();
+//     }
+//     println!("Final Count: {}", *counter.lock().unwrap());
+// }
+//
+//
+use std::sync::{Arc, Mutex}; 
 use std::thread;
+use std::time::Instant;
 
 fn main() {
     let counter = Arc::new(Mutex::new(0));
+    let mut threads = vec![];
 
-    let mut handles: Vec<_> = vec![];
+    let start = Instant::now();
 
-    for i in 1..=2 {
+    for i in 1..=8 {
         let counter_clone = Arc::clone(&counter);
 
-        let handle = thread::spawn(move || {
+        let t = thread::spawn(move || {
             loop {
-                let mut num = counter_clone.lock().unwrap();
+                let mut num= counter_clone.lock().unwrap();
                 if *num >= 100 {
                     break;
                 }
@@ -19,12 +52,20 @@ fn main() {
                 println!("Thread {} increased counter to: {}", i, *num);
             }
         });
-        handles.push(handle);
+        threads.push(t);
+
     }
 
-    for handle in handles {
-        handle.join().unwrap();
+    for thread in threads {
+        thread.join().unwrap();
     }
-    println!("Final Count: {}", *counter.lock().unwrap());
+
+
+    let duration = start.elapsed();
+    println!("Time elapsed: {:?}", duration);
+    // 2 thread,  141.062µs
+    // 4 threads, 147.531µs
+
+    // t1.join().unwrap();
+    // t2.join().unwrap();
 }
-
